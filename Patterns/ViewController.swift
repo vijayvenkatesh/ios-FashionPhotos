62716438f39ace6c0d779d932940d2444b2ceb3b
vijay <vijayv@pobox.com>
2015-07-05 23:45:52 -0400

    Initial Commit

---
+++ Patterns/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Patterns
+//
+//  Created by VIJAY VENKATESH on 7/5/15.
+//  Copyright (c) 2015 VIJAY VENKATESH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
